88e1b5c2d4921a325fe7a97356259e9ef471767c
Jun <lysenef@gmail.com>
2018-02-04 21:06:46 +0900

    add credit card

--- src/selenium_web_driver_test/ShoppingBasketTest.java
+++ src/selenium_web_driver_test/ShoppingBasketTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ShoppingBasketTest extends BaseTest {
@@ -52,10 +53,23 @@ public class ShoppingBasketTest extends BaseTest {
 		givenShopperGoesOnPaymentWithNoneMember(driver);
 		// Credit card check
 		element = driver.findElement(By.id("payment_card_add_306273"));
-		assertThat(element.getAttribute("value"), is("10000"));
+		assertThat(element.getAttribute("value"), is(TestConfigurations.PAYMENT_CREDIT_CARD_ID));
 		// Delivery
 		element = driver.findElement(By.id("delivery_306273_100"));
 		assertThat(element.getAttribute("data-delivery-name"), is("宅配便"));
+		// Credit card add
+		Select select = new Select(element = driver.findElement(By.id("cardRegist_306273")));
+		select.selectByValue("1");
+		driver.findElement(By.name("card_number1")).sendKeys("4444");
+		driver.findElement(By.name("card_number2")).sendKeys("4444");
+		driver.findElement(By.name("card_number3")).sendKeys("4444");
+		driver.findElement(By.name("card_number4")).sendKeys("4448");
+		select = new Select(element = driver.findElement(By.name("card_exp_month")));
+		select.selectByValue("01");
+		select = new Select(element = driver.findElement(By.name("card_exp_year")));
+		select.selectByValue("2020");
+		driver.findElement(By.name("card_owner")).sendKeys("Rakuten");
+		driver.findElement(By.name("addNewCard")).click();
 	}
 	
 	private void givenShopperGoesOnItemPage(){
